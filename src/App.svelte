74fd05c2a88f5577b7baae03c3fd6682cd9f7cef
saintabomination <saint.abomination@protonmail.com>
2022-04-24 16:20:46 +0200

    Adding todo to an array and content updates

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,24 @@
 <script>
-	export let text;
+  import Navigation from './Navigation.svelte';
+  import Footer from './Footer.svelte';
+
+  let todos = [
+    'Todo',
+    'Todo',
+    'Todo',
+  ];
+
+  const addTodo = () => {
+    todos = [...todos, 'Todo'];
+  }
 </script>
 
 <main>
-	<h1>{text}!</h1>
+    <Navigation/>
+    <h1>My To-Do App</h1>
+    <p>{todos}</p>
+    <button on:click={addTodo}>Add Todo</button>
+    <Footer/>
 </main>
 
 <style>
