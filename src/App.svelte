7b6081400e49ef85906c5ced7b74f6839d28f741
saintabomination <saint.abomination@protonmail.com>
2022-04-24 16:57:40 +0200

    Converting todo items into <li> elements

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,20 @@
   import Footer from './Footer.svelte';
 
   let newTodo = '';
-  let todos = [];
+  let todos = [
+    { id: 0, text: 'Todo 1', done: false },
+    { id: 1, text: 'Todo 2', done: false },
+    { id: 2, text: 'Todo 3', done: false },
+    { id: 3, text: 'Todo 4', done: false },
+    { id: 4, text: 'Todo 5', done: false },
+  ];
 
   const addTodo = () => {
-    todos = [...todos, newTodo];
+    todos = [...todos, {
+      id: Math.floor(Math.random() * 1000),
+      text: newTodo,
+      done: false,
+    }];
     newTodo = '';
   }
 
@@ -18,7 +28,15 @@
 <main>
     <Navigation/>
     <h1>My To-Do App</h1>
-    <p>{todos}</p>
+    {#if todos.length}
+      <ul>
+        {#each todos as todo (todo.id)}
+          <li>{todo.text}</li>
+        {/each}
+      </ul>
+      {:else}
+      <p>There are no to-dos to show.</p>
+    {/if}
     <input on:change={e => handleTodoChange(e)} value={newTodo} type="text">
     <button on:click={() => addTodo()}>Add Todo</button>
     <Footer/>
