9be366ad2b1348908ce34f27e17ad6cf166ae41f
saintabomination <saint.abomination@protonmail.com>
2022-04-24 17:44:46 +0200

    Creating Todo component

--- src/App.svelte
+++ src/App.svelte
@@ -2,18 +2,21 @@
   import { v4 as uuidv4 } from 'uuid';
 
   import Navigation from './Navigation.svelte';
+  import Todo from './components/Todo.svelte';
   import Footer from './Footer.svelte';
 
   let newTodo = '';
   let todos = [];
 
   const addTodo = () => {
-    todos = [...todos, {
-      id: uuidv4(),
-      text: newTodo,
-      done: false,
-    }];
-    newTodo = '';
+    if (newTodo) {
+      todos = [...todos, {
+        id: uuidv4(),
+        text: newTodo,
+        done: false,
+      }];
+      newTodo = '';
+    }
   }
 
   const deleteTodo = id => {
@@ -29,15 +32,12 @@
 </script>
 
 <main>
-    <Navigation/>
+    <Navigation />
     <h1>My To-Do App</h1>
     {#if todos.length}
       <ul>
         {#each todos as todo (todo.id)}
-          <li>
-            {todo.text}
-            <button on:click={() => deleteTodo(todo.id)}>Delete</button>
-          </li>
+          <Todo todo={todo} deleteTodo={deleteTodo} />
         {/each}
       </ul>
       {:else}
@@ -45,7 +45,7 @@
     {/if}
     <input on:change={e => handleTodoChange(e)} value={newTodo} type="text">
     <button on:click={() => addTodo()}>Add Todo</button>
-    <Footer/>
+    <Footer />
 </main>
 
 <style>
---
+++ src/components/Todo.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let todo;
+  export let deleteTodo;
+</script>
+
+<div class="todo">
+  <li>
+    {todo.text}
+    <button on:click={() => deleteTodo(todo.id)}>Delete</button>
+  </li>
+</div>
+
+<style>
+
+</style>
